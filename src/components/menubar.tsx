7cabc2de1233181aa3093b31498a9525412b3236
dazcalifornia <divertzt@gmail.com>
2022-10-25 22:22:51 +0700

    menu bar

--- src/components/menubar.tsx
+++ src/components/menubar.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import {
+  Text
+} from 'native-basev';
 export default function Menubar() {
   return (
     <div>
       <h1>Menubar</h1>
+      <Text>Menubar</Text>
     </div>
   );
 }
